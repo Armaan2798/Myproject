7c144ea6ddb3d48a88bdbac6df2a2693f1c18acb
Armaan2798 <108068129+Armaan2798@users.noreply.github.com>
2023-07-27 13:51:46 +0530

    Commit

--- PhoneSimulator/src/edu/jsp/PhoneSimulatorServelet/Delete.java
+++ PhoneSimulator/src/edu/jsp/PhoneSimulatorServelet/Delete.java
@@ -10,22 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.jsp.PhoneSimulatorServelet.dao.PhoneDatabaseOperation;
 
-public class Delete extends HttpServlet
-{
+public class Delete extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		
+
 		resp.setContentType("text/html");
-		PrintWriter out =resp.getWriter();
-		String Number =req.getParameter("id");
-		PhoneDatabaseOperation db =new PhoneDatabaseOperation();
+		PrintWriter out = resp.getWriter();
+		String Number = req.getParameter("id");
+		PhoneDatabaseOperation db = new PhoneDatabaseOperation();
 		db.delete(Number);
 		req.getRequestDispatcher("User").forward(req, resp);
-		
-		
-		
-	}
 
+	}
 
 }
